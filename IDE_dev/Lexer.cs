5444c1e230dc7298453ebd07d5afb3e3fffaf390
krikun98 <krikun98@gmail.com>
2021-05-22 17:07:46 +0300

    C# wrapper and tests

---
+++ IDE_dev/Lexer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Antlr4.Runtime;
+
+namespace IDE_dev
+{
+    public static class Lexer
+    {
+        public static IEnumerable<IToken> Lex(string input)
+        {
+            var tokens = new CommonTokenStream(new Pascal(new AntlrInputStream(input)));
+            tokens.Fill();
+            return tokens.GetTokens();
+        }
+    }
+}
